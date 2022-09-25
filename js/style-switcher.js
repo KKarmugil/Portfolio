a4d048a85e57645be9b7ad96dba3d644cbab5c2e
karmugil <karmugilrc@gmail.com>
2022-09-25 12:41:26 -0700

    index.html

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -24,3 +24,22 @@ function setActiveStyle(color)
 
     })
 }
+//theme
+document.querySelector(".day-night").addEventListener("click",()=>{
+    document.querySelector(".day-night").querySelector("i").classList.toggle("fa-sun");
+    document.querySelector(".day-night").querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark")
+})
+window.addEventListener("load", () => {
+    
+    if(document.body.classList.contains("dark"))
+    {
+        console.log("dark")
+        document.querySelector(".day-night").querySelector("i").classList.add("fa-sun");
+    }
+    else
+    {
+        console.log("light")
+        document.querySelector(".day-night").querySelector("i").classList.add("fa-moon")
+    }
+})
